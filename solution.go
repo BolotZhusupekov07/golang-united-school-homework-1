c2bb965d57c3a3f45c2852c603ca17bc1af3aa37
Bolot Zhusupekov <73470679+BolotZhusupekov07@users.noreply.github.com>
2022-02-14 23:08:30 +0600

    fix

--- solution.go
+++ solution.go
@@ -1,16 +1,9 @@
-package main
+package solution
 
 import (
-	"fmt"
-
 	"github.com/kyokomi/emoji/v2"
 )
 
 func GetMessage() string {
 	return emoji.Sprint("Hello :world_map:!")
-}
-
-func main() {
-	message := GetMessage()
-	fmt.Println(message)
 }
